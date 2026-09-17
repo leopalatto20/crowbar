acea13b0f6dda87fcb04ed7b1fcc6b933e6899d1
Leonardo Perez <A01665462@tec.mx>
2026-09-17 16:45:38 -0600

    chore: install core dependencies

--- components/ui/gluestack-ui-provider/index.tsx
+++ components/ui/gluestack-ui-provider/index.tsx
@@ -1,30 +1,24 @@
-import React, { useEffect } from 'react';
-import { View, ViewProps } from 'react-native';
-import { OverlayProvider } from '@gluestack-ui/core/overlay/creator';
-import { ToastProvider } from '@gluestack-ui/core/toast/creator';
-import { Appearance, ColorSchemeName } from "react-native";
+import React, { useEffect } from "react";
+import { View, ViewProps, Appearance, ColorSchemeName } from "react-native";
+import { OverlayProvider } from "@gluestack-ui/core/overlay/creator";
+import { ToastProvider } from "@gluestack-ui/core/toast/creator";
 
-export type ModeType = 'light' | 'dark' | 'system';
+export type ModeType = "light" | "dark" | "system";
 
 export function GluestackUIProvider({
-  mode = 'system',
+  mode = "system",
   ...props
 }: {
   mode?: ModeType;
   children?: React.ReactNode;
-  style?: ViewProps['style'];
+  style?: ViewProps["style"];
 }) {
   useEffect(() => {
     Appearance.setColorScheme(mode as ColorSchemeName);
   }, [mode]);
 
   return (
-    <View
-      style={[
-        { flex: 1, height: '100%', width: '100%' },
-        props.style,
-      ]}
-    >
+    <View style={[{ flex: 1, height: "100%", width: "100%" }, props.style]}>
       <OverlayProvider>
         <ToastProvider>{props.children}</ToastProvider>
       </OverlayProvider>
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativewind } = require('nativewind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativewind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
