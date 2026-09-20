04fed3b0d437556901d5923c26511fb4c6c491bf
Leonardo Perez <A01665462@tec.mx>
2026-09-19 20:03:53 -0600

    chore: install core dependencies

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,14 +5,21 @@ import { useColorScheme } from 'react-native';
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/src/global.css';
+
 SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    
+    <GluestackUIProvider mode="dark">
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
       <AppTabs />
     </ThemeProvider>
+    </GluestackUIProvider>
+  
   );
 }
